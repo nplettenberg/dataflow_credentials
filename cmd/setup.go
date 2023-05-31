2a9493dd2ad117ad7c9cd3f0a570a03c90ec2e59
Nino Plettenberg <nino.plettenberg@plentymarkets.com>
2023-05-31 18:57:27 +0200

    chore: remove fallbacks and throw if error if configuration is missing

--- cmd/setup.go
+++ cmd/setup.go
@@ -7,9 +7,13 @@ import (
 
 func CreateMinioConnection() (*minio.Client, error) {
 
-	minioEndpoint := GetEnv("MINIO_ENDPOINT", "localhost:9001")
-	minioKey := GetEnv("MINIO_ACCESS_KEY", "0t7nB2RIqguQXTnPwRTY")
-	minioSecret := GetEnv("MINIO_ACCESS_SECRET", "qe1AD0ahdB1eDwgL9uCA3pxjChC4E5Shfpmx2og8")
+	minioEndpoint := GetEnv("MINIO_ENDPOINT", "")
+	minioKey := GetEnv("MINIO_ACCESS_KEY", "")
+	minioSecret := GetEnv("MINIO_ACCESS_SECRET", "")
+
+	if len(minioEndpoint) == 0 || len(minioKey) == 0 || len(minioSecret) == 0 {
+		panic("Missing connection information for MinIO")
+	}
 
 	return minio.New(minioEndpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(minioKey, minioSecret, ""),
