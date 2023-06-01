2bbe066325a53dae1b95d8dae653e0e76e574c1d
Nino Plettenberg <nino.plettenberg@plentymarkets.com>
2023-06-01 12:35:33 +0200

    chore: add startup log

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
@@ -10,6 +12,8 @@ func CreateMinioConnection() (*minio.Client, error) {
 	minioEndpoint := GetEnv("MINIO_ENDPOINT", "")
 	minioKey := GetEnv("MINIO_ACCESS_KEY", "")
 	minioSecret := GetEnv("MINIO_ACCESS_SECRET", "")
+
+	fmt.Printf("Connecting to minio:\n Endpoint: %s \n KeyLenght: %d \n SecretLenght: %d \n", minioEndpoint, len(minioKey), len(minioSecret))
 
 	if len(minioEndpoint) == 0 || len(minioKey) == 0 || len(minioSecret) == 0 {
 		panic("Missing connection information for MinIO")
